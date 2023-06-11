f4713791c5983f325457e922d8ccc73f2faecbd9
hossine-dev <104509129+hossine-dev@users.noreply.github.com>
2023-06-12 00:39:48 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,216 +1,171 @@
-#include "minishell.h"
-
-void add_token(t_list **list, char *token, enum token type);
 void get_token(t_list **list, char *line);
 
 
 char *ftft(const char *s, unsigned int start, size_t len)
 {
-    char *substr = (char *)malloc(len + 1);
-    strncpy(substr, s + start, len);
-    substr[len] = '\0';
-    return substr;
+	char *substr = (char *)malloc(len + 1);
+	strncpy(substr, s + start, len);
+	substr[len] = '\0';
+	return substr;
+}
+int ft_islogical(char c)
+{
+	if (c == '|' || c == '>' || c == '<' || c == '$' || c == '&' || c == '\n'
+		|| c == '\'' || c == '\"' || c == ' ' || c == '\t' || c == '\0')
+		return (1);
+	return (0);
+}
+int ft_isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (c);
+	return (0);
 }
 
-// char *first_token(char *line)
-// {
-//     int i;
-//     char *token;
-
-//     i = 0;
-//     while (line[i] != '\0' && (line[i] != ' '))
-//         i++;
-//     token = malloc(sizeof(char) * (i + 1));
-//     i = 0;
-//     while (line[i] != '\0' && line[i] != ' ')
-//     {
-//         token[i] = line[i];
-//         i++;
-//     }
-//     token[i] = '\0';
-//     return (token);
-// }
-
-// char **get_line(char *line)
-// {
-//     char **tab;
-//     int i;
-//     int j;
-//     int k;
-
-//     i = 0;
-//     j = 0;
-//     k = 0;
-//     tab = malloc(sizeof(char *) * (ft_strlen(line) + 1));
-//     while (line[i] != '\0')
-//     {
-//         if (line[i] == ' ')
-//         {
-//             tab[j] = malloc(sizeof(char) * (i + 1));
-//             while (k < i)
-//             {
-//                 tab[j][k] = line[k];
-//                 k++;
-//             }
-//             tab[j][k] = '\0';
-//             j++;
-//             k = 0;
-//         }
-//         i++;
-//     }
-//     tab[j] = malloc(sizeof(char) * (i + 1));
-//     while (k < i)
-//     {
-//         tab[j][k] = line[k];
-//         k++;
-//     }
-//     tab[j][k] = '\0';
-//     return (tab);
-// }
-// int ft_isalpha(int c)
-// {
-//     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-//         return (c);
-//     return (0);
-// }
-
-
-// char *get_rest(char *line)
-// {
-//     char *rest;
-//     rest = ft_substr(line, ft_strlen(first_token(line)), ft_strlen(line));
-//     return (rest);
-// }
-
-
-void get_token( t_list **list , char *line)
+void get_token(t_list **list,char *line)
 {
-    int i;
-    int j;
-    int state;
-    state =  1;
-    i = 0;
+	int i;
+	int j;
+	char *p;
 
-    while (line[i] != '\0')
-    {
-        if(line[i] == '"')
-           state = 0;
-    
-        else
-            state = 1;
-        
-        if(state ==1)
-        {
-        if (line[i] == '|')
-            {
-                add_token(list, "|", PIPE_LINE);
-            }
-            else if (line[i] == '>')
-            {
-                add_token(list, ">", REDIR_OUT);
-            }
-            else if (line[i] == '<')
-            {
-                add_token(list, "<", REDIR_IN);
-            }
-            else if (line[i] == '$')
-            {
-                add_token(list, "$", ENV);
-            }
-            else if (line[i] == '\'')
-            {
-                add_token(list, "\'", QOUTE);
-            }
-            else if (line[i] == '\n')
-            {
-                add_token(list, "\n", NEW_LINE);
-            }
-            else if (line[i] == ' ')
-            {
-                add_token(list, " ", WHITE_SPACE);
-            }
-            else
-            {
-                j = i;
+	i = 0;
 
-                while(line[i] != ' ' && line[i] != '\0')
-                    i++;
-                add_token(list, ft_substr(line, j, i - j), WORD);
+	while(line[i])
+	{
+			if(!ft_islogical(line[i]))
+			{
+				j = i;
+				while(!ft_islogical(line[i]) && line[i] != '\0') 
+					i++;
 
-                if(line[i] == ' ')
-                    add_token(list, " ", WHITE_SPACE);
-            }
-        }
-        else
-        {
-            if (line[i] == '"')
-            {
-                add_token(list, "\"", DOUBLE_QUOTE);
-            }
-            else
-            {
-                j = i;
-
-                while(line[i] != '"' && line[i] != '\0')
-                    i++;
-                add_token(list, ft_substr(line, j, i - j), WORD);
-
-                if(line[i] == '"')
-                    add_token(list, "\"", DOUBLE_QUOTE);
-            }
-        }
-        i++;
-    }
+				p = ftft(line, j, i - j);
+				add_token(list, p, WORD, GENERAL);
+			}
+				if(line[i] == '|')
+					add_token(list, "|", PIPE_LINE, GENERAL);
+				if(line[i] == '>')
+				{
+					p = ft_substr(line, i, ft_strlen(line));
+					if(line[i + 1] == '>')
+					{
+						add_token(list, ">>", APPEND, GENERAL);
+						i++;
+					}
+					else
+						add_token(list, ">", REDIR_OUT, GENERAL);
+				}
+				if(line[i] == '<')
+				{
+					p = ft_substr(line, i, ft_strlen(line));
+					if(line[i + 1] == '<')
+					{
+						add_token(list, "<<", HEARDOC, GENERAL);
+						i++;
+					}
+					else
+						add_token(list, "<", REDIR_IN, GENERAL);
+				}
+				if(line[i] == '$')
+					add_token(list, "$", ENV, GENERAL);
+			
+				if(line[i] == '\'')
+				{
+					add_token(list, "\'", QUOTE, IN_SQUOTE);
+					i++;
+					j = i;
+					while(line[i] != '\'')
+						i++;
+					p = ftft(line, j, i - j);
+					add_token(list, p, WORD, IN_SQUOTE);
+					add_token(list, "\'", QUOTE, IN_SQUOTE);
+				}
+				if(line[i] == '\n')
+					add_token(list, "\n", NEW_LINE, GENERAL);
+				if(line[i] == '&')
+					add_token(list, "&", AND, GENERAL);
+				if(line[i] == ' ')
+					add_token(list, " ", WHITE_SPACE, GENERAL);
+				if(line[i] == '\"')
+				{
+					add_token(list, "\"", DOUBLE_QUOTE, IN_DQUOTE);
+					i++;
+					j = i;
+					while(line[i] != '\"' && line[i] != '\0')
+					{
+						i++;
+					}
+					if(line[i] == '\0')
+					{
+						printf("Error: unclosed double quote\n");
+						exit(1);
+					}
+					if(i > j)
+					{
+						p = ftft(line, j, i - j);
+						add_token(list, p, WORD, IN_DQUOTE);
+					}
+					if(line[i] == '\"')
+						add_token(list, "\"", DOUBLE_QUOTE, IN_DQUOTE);				
+				}
+					
+		i++;
+	}
 }
 
-
-t_list *ft_lstnew(char *content, enum token type)
+t_list *ft_lstnew(char *content, enum token type, enum state state)
 {
-    t_list *list;
+	t_list *list;
 
-    list = malloc(sizeof(t_list));
-    if (!list)
-        return (NULL);
-    list->content = content;
-    list->token = type;
-    list->next = NULL;
-    return (list);
+	list = malloc(sizeof(t_list));
+	if (!list)
+		return (NULL);
+	list->content = content;
+	list->token = type;
+	list->state = state;
+	list->next = NULL;
+	return (list);
 }
 
-void add_token(t_list **list, char *token, enum token type)
+void add_token(t_list **list, char *token, enum token type, enum state state)
 {
-    t_list *new;
+	t_list *new;
 
-    new = ft_lstnew(token, type);
-    if(new != NULL)
-        ft_lstadd_back(list, new);
+	new = ft_lstnew(token, type, state);
+	if(new != NULL)
+		ft_lstadd_back(list, new);
 }
 
 int main(int ac, char **av)
 {
-    char *line;
+	char *line;
 
-    t_list *list;
+	t_list *list;
 
-    list = NULL;
+	list = NULL;
+	enum token type;
 
-    int i = 0;
-    (void)ac;
+	int i = 0;
+	(void)ac;
+	const char *token[] = 
+	{"WORD", "WHITE_SPACE", "NEW_LINE", "QUOTE", "DOUBLE_QUOTE", "ENV", "PIPE_LINE", "REDIR_IN", "REDIR_OUT", "AND", "HEARDOC", "APPEND"};
+	const char *state[] = {"IN_DQUOTE", "IN_SQUOTE", "GENERAL"};
+	while (1)
+	{
+		line = readline("minishell-> ");
+			if (line == NULL)
+				break ;
+			get_token(&list, line);
+			syntax_check(&list);
+			while (list)
+			{
+				printf("----------------------\n");
+				printf("content: '%s'\n", list->content);
+				printf("token: %s\n", token[list->token]);
+				printf("state: %s\n", state[list->state]);
+				printf("----------------------\n");
+				list = list->next;
+			}
+	}
 
-    while (1)
-    {
-        line = readline("minishell-> ");
-            if (line == NULL)
-                break ;
-            get_token(&list, line);
-
-            while (list)
-            {
-                printf("list  '%s'\n", list->content);
-                // printf("type %d\n", list->token);
-                list = list->next;
-            }
-    }
-
-    return 0;
+	return 0;
 }
