d071cce58a6e4b2af8159f3ff0c494ce5829c6b3
hossine-dev <104509129+hossine-dev@users.noreply.github.com>
2023-06-12 00:53:20 +0100

    Update minishell.h

--- minishell.h
+++ minishell.h
@@ -1,6 +1,6 @@
 
-#ifndef MINISHELLL_H
-# define MINISHELLL_H
+#ifndef MINISHELL_H
+# define MINISHELL_H
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
@@ -11,21 +11,25 @@
 enum state
 {
 	IN_DQUOTE,
-	IN_QUOTE,
+	IN_SQUOTE,
+
 	GENERAL,
 };
 
 enum token
 {
-	WORD = -1,
-	WHITE_SPACE = ' ',
-	NEW_LINE = '\n',
-	QOUTE = '\'',
-	DOUBLE_QUOTE = '\"',
-	ENV = '$',
-	PIPE_LINE = '|',
-	REDIR_IN = '<',
-	REDIR_OUT = '>',
+	WORD,
+	WHITE_SPACE ,
+	NEW_LINE,
+	QUOTE ,
+	DOUBLE_QUOTE ,
+	ENV,
+	PIPE_LINE ,
+	REDIR_IN ,
+	REDIR_OUT,
+	AND ,
+	HEARDOC ,	
+	APPEND ,	
 } ;
 
 typedef struct s_list
@@ -37,12 +41,13 @@ typedef struct s_list
 }					t_list;
 
 char **ft_split(char const *s, char c);
-
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlen(const char *s);
-t_list	*ft_lstnew(char *content, enum token type);
-
+t_list	*ft_lstnew(char *content, enum token type, enum state state);
+void add_token(t_list **list, char *token, enum token type, enum state state);
+void syntax_check(t_list **list);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 # endif
