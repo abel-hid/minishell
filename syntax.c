014410e1c87cb9bf50af306e5bc7f57273e1b1d3
hossine-dev <104509129+hossine-dev@users.noreply.github.com>
2023-06-12 00:38:56 +0100

    Create syntax.c

---
+++ syntax.c
@@ -0,0 +1,47 @@
+#include "minishell.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!(s1[i] || s2[i]))
+		return (0);
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
+int ft_isnological(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+    if (str[i] == '|' || str[i] == '>' || str[i] == '<'  || str[i] == '&' 
+    || str[i] == '$' || str[i] == '\'' || str[i] == '\"')
+            return (1);
+        i++;
+    }
+    return (0);
+}
+void syntax_check(t_list **list)
+{
+  
+    t_list *tmp;
+    tmp = *list;
+    while (tmp != NULL)
+    {
+        if (ft_isnological(tmp->content) == 1)
+        {
+            printf("syntax error near unexpected token `%s'\n", tmp->content);
+            exit(1);
+        }
+        tmp = tmp->next;
+    }
+    
+}
