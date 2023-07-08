54aca7550c32b20eea018367ed6fecee331eb979
Abdeljalil El Hidaoui <abel-hid@e2r2p13.1337.ma>
2023-07-08 22:25:06 +0100

    edit

--- expander.c
+++ expander.c
@@ -252,7 +252,7 @@ int detect_dollar(char *str)
 	int i = 0;
 	while(str[i])
 	{
-		if(str[i] == '$' && str[i + 1] != '$' && str[i + 1] != '\0' && str[i + 1] != '\"')
+		if(str[i] == '$' && str[i + 1] != '$' && str[i + 1] != '\0' )
 			return (1);
 		i++;
 	}
@@ -339,6 +339,7 @@ char *expand_variables(t_lexer **list, t_env **g_env)
 			{
 				if(detect_dollar(tmp->content) && !if_dollar_out_quote(tmp->content))
 				{
+					printf("tmp->content : %s\n", tmp->content);
 					tmp->content = ft_expand(tmp->content, g_env);
 				}
 			}
