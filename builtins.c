5a1f7b44a9b4976ea3584c15ef89ae914b6a0ab9
Hossine Eddahbi <heddahbi@e3r9p11.1337.ma>
2023-07-09 15:12:51 +0100

    update the builtin system

--- builtins.c
+++ builtins.c
@@ -3,146 +3,272 @@
 #include <string.h>
 #include <unistd.h>
 #include <dirent.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <dirent.h>
+#include <signal.h>
 
 void _ls()
 {
-     DIR *dir;
-    struct dirent *entry;
+	 DIR *dir;
+	struct dirent *entry;
 
-    dir = opendir(".");
-    if (dir == NULL) {
-        perror("opendir");
-    }
+	dir = opendir(".");
+	if (dir == NULL) {
+		perror("opendir");
+	}
 
-    while ((entry = readdir(dir)) != NULL) {
-        printf("%s ", entry->d_name);
-    }
-    closedir(dir);
+	while ((entry = readdir(dir)) != NULL) {
+		printf("%s ", entry->d_name);
+	}
+	printf("\n");
+	closedir(dir);
 
 }
 int ft_strcmp(const char *s1, const char *s2)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (s1[i] && (s1[i] == s2[i]))
-        i++;
-    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	i = 0;
+	while (s1[i] && (s1[i] == s2[i]))
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 char **ft_split(char *str, char c)
 {
-    char **tab;
-    int i;
-    int j;
-    int k;
+	char **tab;
+	int i;
+	int j;
+	int k;
 
-    i = 0;
-    j = 0;
-    tab = malloc(sizeof(char *) * 100);
-    while (str[i])
-    {
-        k = 0;
-        tab[j] = malloc(sizeof(char) * 100);
-        while (str[i] && str[i] != c)
-        {
-            tab[j][k] = str[i];
-            i++;
-            k++;
-        }
-        tab[j][k] = '\0';
-        j++;
-        if (str[i])
-            i++;
-    }
-    tab[j] = NULL;
-    return (tab);
+	i = 0;
+	j = 0;
+	tab = malloc(sizeof(char *) * 100);
+	while (str[i])
+	{
+		k = 0;
+		tab[j] = malloc(sizeof(char) * 100);
+		while (str[i] && str[i] != c)
+		{
+			tab[j][k] = str[i];
+			i++;
+			k++;
+		}
+		tab[j][k] = '\0';
+		j++;
+		if (str[i])
+			i++;
+	}
+	tab[j] = NULL;
+	return (tab);
 }
 
 void _env()
 {
-    extern char **environ;
-    int i;
+	extern char **environ;
+	int i;
 
-    i = 0;
-    while (environ[i])
-    {
-        printf("%s\n", environ[i]);
-        i++;
-    }
+	i = 0;
+	while (environ[i])
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 void _pwd()
 {
-    char cwd[1024];
+	char cwd[1024];
 
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-        printf("%s\n", cwd);
-    else
-        perror("getcwd() error");
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		printf("%s\n", cwd);
+	else
+		perror("getcwd() error");
 
+	printf("\n");
 }
-void echo (char *args)
+void _cd(char *args)
 {
     int i;
 
     i = 0;
-    while (args[i])
-    {
-        printf("%c", args[i]);
+    while (args[i] == ' ')
         i++;
-    }
+    if (chdir(&args[i]) != 0)
+        perror("chdir");
+    else
+        printf("Directory changed to: %s\n", &args[i]);
+
     printf("\n");
+}
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+			return (1);
+	return (0);
+}
+
+int ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+void echo (char *args)
+{
+	int i;
+
+	i = 0;
+	while (args[i])
+	{
+		printf("%c", args[i]);
+		i++;
+	}
+	printf("\n");
+}
+void export(char *str)
+{
+	extern char **environ;
+	char **tab;
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	tab = malloc(sizeof(char *) * 100);
+	while (environ[i])
+	{
+		tab[j] = malloc(sizeof(char) * 100);
+		while (environ[i][k])
+		{
+			tab[j][k] = environ[i][k];
+			k++;
+		}
+		tab[j][k] = '\0';
+		j++;
+		i++;
+	}
+	tab[j] = malloc(sizeof(char) * 100);
+	k = 0;
+	while (str[k])
+	{
+		tab[j][k] = str[k];
+		k++;
+	}
+	tab[j][k] = '\0';
+	tab[j + 1] = NULL;
+	environ = tab;
+}
+void _unset(char *str)
+{
+	extern char **environ;
+	char **tab;
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	tab = malloc(sizeof(char *) * 100);
+	while (environ[i])
+	{
+		if (ft_strcmp(environ[i], str) != 0)
+		{
+			tab[j] = malloc(sizeof(char) * 100);
+			while (environ[i][k])
+			{
+				tab[j][k] = environ[i][k];
+				k++;
+			}
+			tab[j][k] = '\0';
+			j++;
+		}
+		i++;
+	}
+	tab[j] = NULL;
+	environ = tab;
 }
 
 void _builtin_handler(char *str)
 {
-    char **args;
-    int i;
+	char **args;
+	int i;
 
-    i = 0;
-    while(str[i])
-    {
-        while(str[i] == ' ')
-            i++;
-        if (str[i] == 'l' && str[i + 1] == 's')
-            _ls();
-        else if (str[i] == 'p' && str[i + 1] == 'w' && str[i + 2] == 'd')
-            _pwd();
-        else if (str[i] == 'e' && str[i + 1] == 'c' && str[i + 2] == 'h' && str[i + 3] == 'o')
-        {
-            echo(&str[i + 4]);
-            i += 4;
-        }
-        else if (str[i] == 'e' && str[i + 1] == 'n' && str[i + 2] == 'v')
-            _env();
-        else if (str[i] == 'e' && str[i + 1] == 'x' && str[i + 2] == 'i' && str[i + 3] == 't')
-            exit(0);
-        i++;
-    }
+	i = 0;
+	while(str[i])
+	{
+		while(str[i] == ' ')
+			i++;
+		if (str[i] == 'l' && str[i + 1] == 's' && (str[i + 2] == '\n'|| str[i + 2] == ' '))
+		{
+			if(str[i] - 1 != ' ' || _isalpha(str[i] - 1))
+			_ls();
+		}
+		else if (str[i] == 'p' && str[i + 1] == 'w' && str[i + 2] == 'd')
+			_pwd();
+		else if (str[i] == 'e' && str[i + 1] == 'c' && str[i + 2] == 'h' && str[i + 3] == 'o')
+		{
+			echo(&str[i + 4]);
+			i += 4;
+
+		}
+		else if (str[i] == 'e' && str[i + 1] == 'n' && str[i + 2] == 'v')
+			_env();
+		else if(str[i] == 'c' && str[i + 1] == 'd')
+		{
+			_cd(&str[i + 3]);
+			i += 3;
+		}
+		else if (str[i] == 'c' && str[i + 1] == 'l' && str[i + 2] == 'e' && str[i + 3] == 'a' && str[i + 4] == 'r')
+			system("clear");
+		else if (str[i] == 'e' && str[i + 1] == 'x' && str[i + 2] == 'i' && str[i + 3] == 't')
+			exit(0);
+		else if(str[i] == 'u' && str[i + 1] == 'n' && str[i + 2] == 's' && str[i + 3] == 'e' && str[i + 4] == 't')
+		{
+			_unset(&str[i + 6]);
+			i += 6;
+		}
+		i++;
+	}
 }
+void signal_handler(int sig)
+{
+	if(sig == SIGINT)
+	{
+		printf("\n");
+		printf("minishell-> ");
+		fflush(stdout);
+
+	}
+	else if (sig == SIGQUIT || sig == SIGSTOP)
+	{
+		printf("Quit (core dumped)\n");
+		exit(0);
+	}
+
+	
+
+}
+
 
 
 
 int main(int ac , char **av)
 {
-    char *line;
-    char **args;
-    int i;
+	char *line;
+	char **args;
+	int i;
 
-    i = 1;
-    (void)ac;
-    (void)av;
-    while (1)
-    {
-        printf("\nminishell-> ");
-        getline(&line, (size_t*)&i, stdin);
-        _builtin_handler(line);
-    }
-    return (0);
+	i = 1;
+	(void)ac;
+	(void)av;
+		signal(SIGINT, signal_handler);
+	while (1)
+	{
+		printf("minishell-> ");
+		getline(&line, (size_t*)&i, stdin);
+		_builtin_handler(line);
+	}
+	return (0);
 }
