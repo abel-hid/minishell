bb2ef46fd44c284e7545d097220f089c367fb998
Abdeljalil El Hidaoui <abel-hid@e3r6p7.1337.ma>
2023-07-15 15:56:04 +0100

    key

--- parsing.c
+++ parsing.c
@@ -1,128 +1,25 @@
 #include "minishell.h"
 
-// t_command *ft_new(char **content, int  fd, t_redir_list *redir_list)
-// {
-// 	t_command *list;
 
-// 	list = malloc(sizeof(t_command));
-// 	if (!list)
-// 		return (NULL);
-// 	list->args = content;
-// 	// list->redir_list = redir_list;
-// 	return (list);
-// }
+t_command	*lstlast(t_command *lst)
+{
+	printf("[%p]\n", lst);
+	while (lst && lst->next)
+		lst = lst->next;
+	return (lst);
+}
+void my_lstadd_back(t_command **lst, t_command *new)
+{
+	t_command	*last_add;
 
-// t_command **srfak_lban(t_lexer **list)
-// {
-// 	t_lexer *tmp;
-// 	t_fd fd;
-
-
-// 	tmp = *list;
-// 	t_command **command = malloc(sizeof(t_command ));
-// 	if (!command)
-// 		return (NULL);
-
-// 	while(tmp)
-// 	{
-// 		if(tmp->token == WORD)
-// 		{
-// 			*command = ft_new(&tmp->content, fd.fd_in, NULL);
-// 		}
-// 		if(tmp->token == REDIR_IN)
-// 		{
-// 			tmp= tmp->next;
-// 			fd.fd_in = open(tmp->content,O_CREAT | O_RDONLY);
-// 			if(fd.fd_in == -1)
-// 			{
-// 				printf("error\n");
-// 				return (NULL);
-// 			}
-// 		}
-// 		if(tmp->token == REDIR_OUT)
-// 		{
-// 			tmp= tmp->next;
-// 			fd.fd_out = open(tmp->content,O_CREAT | O_WRONLY);
-// 			if(fd.fd_out == -1)
-// 			{
-// 				printf("error\n");
-// 				return (NULL);
-// 			}
-// 		}
-
-// 		if(tmp->token == PIPE_LINE)
-// 		{
-// 			tmp = tmp->next;
-// 			*command = ft_new(&tmp->content, fd.fd_in, NULL);
-// 		}
-// 		if(tmp->token == HEARDOC)
-// 		{
-// 			tmp = tmp->next;
-// 			*command = ft_new(&tmp->content, fd.fd_in, NULL);
-// 		}
-// 		tmp = tmp->next;
-// 	}
-// 	list = &tmp;
-// 	return (command);
-// }
-
-// int parsing(t_lexer **list, t_command **command)
-// {
-// 	t_lexer *tmp;
-// 	t_lexer *tmp2;
-// 	t_fd fd;
-// 	t_redir_list *redir_list;
-// 	char **content;
-// 	int i;
-
-
-// 	tmp = *list;
-// }
-
-
-// // 	tmp = *list;
-// // }
-// void itis_acmd(char *str, t_command *cmd)
-// {
-//     int i ; 
-//     i =0;
-//     // int j = 0;
-//     while(str[i])
-//     {
-//         while(str[i] && str[i] != ' '  && str[i] != '|' && str[i] != '<' && str[i] != '>' && str[i] != ';')
-//             i++;
-//         cmd->command_name = malloc(sizeof(char) * (i + 1));
-//         if(!cmd->command_name)
-//             return ;
-//         printf("%s\n", cmd->command_name);
-        
-//     }
-// }
-
-// void _parsing(t_lexer **list, t_command *cmd)
-// {
-//     t_lexer *tmp = *list;
-
-//     if (!tmp)
-//         return;
-
-//     cmd = malloc(sizeof(t_command));
-//     if (!cmd)
-//         return;
-
-//     cmd->command_name = tmp->content;
-//     printf("%s\n", cmd->command_name);
-//     if(tmp->token == WORD)
-//     {
-//         cmd->args = malloc(sizeof(char *) * 2);
-//         if(!cmd->args)
-//             return ;
-//         cmd->args[0] = tmp->content;
-//         cmd->args[1] = NULL;
-//         printf("%s\n", cmd->args[0]);
-//     }
-//     list = &tmp;
-// }
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	last_add = lstlast(*lst);
+	last_add->next = new;
+}
 
 void ft_putendl_fd(char *s, int fd)
 {
@@ -138,91 +35,112 @@ void ft_putendl_fd(char *s, int fd)
 	}
 	write(fd, "\n", 1);
 }
-int is_apuce(int  h)
+
+t_command *ft_new(char **args, t_fd fd)
 {
-	if(h == PIPE_LINE || h == REDIR_IN || h == REDIR_OUT || h == HEARDOC  || h == APPEND)
-		return (1);
-	return (0);
+	t_command *list;
+
+	list = malloc(sizeof(t_command));
+	if (!list)
+		return (NULL);
+	list->args = args;
+	list->fd = fd;
+	list->next = NULL;
+	return (list);
 }
 
+int ft_lstsize(t_lexer *lst)
+{
+	int i;
+
+	i = 0;
+	while (lst != NULL)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
+}
+int calculate_args(t_lexer *tmp)
+{
+	int i;
+
+	i = 0;
+	while (tmp != NULL && tmp->token != PIPE_LINE)
+	{
+		if (tmp->token == WORD)
+			i++;
+		tmp = tmp->next;
+	}
+	return (i);
+}
 
 void _parsing(t_lexer **list, t_command **cmd)
 {
-    t_lexer *tmp = *list;
-    *cmd = malloc(sizeof(t_command));
+	t_lexer *tmp = *list;
+	int i = 0;
 	t_fd fd;
-	char *line;
-    if (!*cmd)
-        return;
-	while(tmp)
+
+	*cmd = NULL;
+	fd.fd_in = 0;
+	fd.fd_out = 1;
+	char **args;
+	int args_size = calculate_args(tmp);
+	args = malloc(sizeof(char *) * (args_size + 1));
+	if(!args)
+		return ;
+
+	while (tmp != NULL)
 	{
-		while(tmp->token ==  HEARDOC)
+		if (tmp->token == WORD)
+		{
+			args[i] = ft_strdup(tmp->content);
+			i++;
+		}
+		else if (tmp->token == REDIR_IN)
 		{
 			tmp = tmp->next;
-			fd.fd_in = open("heredoc", O_CREAT | O_WRONLY | O_APPEND);
-			while(1)
+			fd.fd_in = open(tmp->content, O_RDONLY, 0644);
+			if (fd.fd_in == -1)
 			{
-				line = readline("heredoc> ");
-				if(!ft_strncmp(line, tmp->content, ft_strlen(tmp->content)))
-					break;
-				ft_putendl_fd(line, fd.fd_in);
+				printf("minishell: %s: No such file or directory\n", tmp->content);
+				return;
 			}
 		}
-		// if(tmp->token == REDIR_IN)
-		// {
-		// 	tmp= tmp->next;
-		// 	fd.fd_in = open(tmp->content,O_CREAT | O_RDONLY);
-		// 	if(fd.fd_in == -1)
-		// 	{
-		// 		printf("error\n");
-		// 		return ;
-		// 	}
-		// }
-		// if(tmp->token == REDIR_OUT)
-		// {
-		// 	tmp= tmp->next;
-		// 	fd.fd_out = open(tmp->content,O_CREAT | O_WRONLY);
-		// 	if(fd.fd_out == -1)
-		// 	{
-		// 		printf("error\n");
-		// 		return ;
-		// 	}
-		// }
-		// if(tmp->token == APPEND)
-		// {
-		// 	tmp= tmp->next;
-		// 	fd.fd_out = open(tmp->content,O_CREAT | O_WRONLY | O_APPEND);
-		// 	if(fd.fd_out == -1)
-		// 	{
-		// 		printf("error\n");
-		// 		return ;
-		// 	}
-		// }
+		else if (tmp->token == REDIR_OUT)
+		{
+			tmp = tmp->next;
+			fd.fd_out = open(tmp->content, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+			if (fd.fd_out == -1)
+			{
+				printf("minishell: $s: ambiguous redirect\n");
+				return;
+			}
+		}
+		else if (tmp->token == APPEND)
+		{
+			tmp = tmp->next;
+			fd.fd_out = open(tmp->content, O_CREAT | O_WRONLY | O_APPEND, 0644);
+			if (fd.fd_out == -1)
+			{
+				printf("minishell: $s: ambiguous redirect\n");
+				return;
+			}
+		}
+
+		if (!tmp->next || tmp->next->token == PIPE_LINE)
+		{
+			args[i] = NULL;
+			my_lstadd_back(cmd, ft_new(args, fd));
+			if (tmp->next && tmp->next->token == PIPE_LINE)
+			{
+				args = malloc(sizeof(char *) * (args_size + 1));
+				if(!args)
+					return ;
+			}
+			i = 0;
+		}
 		tmp = tmp->next;
 	}
-	tmp = *list;
-    int args_count = 0;
-    while (tmp)
-    {
-		
-        if (tmp->token == WORD)
-        {
-            (*cmd)->args = malloc(sizeof(char *) * (args_count + 2));
-            if (!(*cmd)->args)
-                return;
-        	(*cmd)->args[args_count] = tmp->content;
-			if( tmp->next && !is_apuce(tmp->next->token))
-					tmp  = tmp->next->next;
-            (*cmd)->args[args_count + 1] = NULL;
-            printf("args %d : %s\n", args_count,(*cmd)->args[args_count]);
-           	args_count++;
-        }
-		
-		
-        tmp = tmp->next;
-    }
 
-    *list = tmp;
-    tmp = NULL;
 }
-
