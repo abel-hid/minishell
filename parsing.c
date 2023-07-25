a77da7d06bdb8d4e049ad852fc729f6dcb720e0f
Abdeljalil El Hidaoui <abel-hid@e3r7p15.1337.ma>
2023-07-25 04:34:15 +0100

    edit

--- parsing.c
+++ parsing.c
@@ -98,128 +98,276 @@ int is_quote1(char *str)
 	return (0);
 }
 
-int process_env(t_lexer *tmp, t_env *env)
+int parse_env(t_env **g_env, char *str)
 {
-	int j = 0;
-	while (env)
+	t_env *env;
+	env = *g_env;
+
+	while(env)
 	{
 		env->value = del_quote(env->value, '\'', '\"');
-		if (ft_strcmp(env->value, tmp->content) == 0)
-			j = 1;
+		if(ft_strcmp(env->value, str) == 0)
+			return (1);
 		env = env->next;
 	}
-	return j;
+	return (0);
 }
 
-
-void parsing(t_lexer **list, t_command **cmd, t_env **g_env)
+char *add_args(char *str)
 {
-	t_lexer *tmp = *list;
-	int i = 0;
-	int j = 0;
-	t_fd fd;
-	int k;
-	t_env *env;
+	str = del_quote(str, '\'', '\"');
+	str = ft_strdup(str);
+	return (str);
+}
 
-	*cmd = NULL;
-	fd.fd_in = 0;
-	fd.fd_out = 1;
-	char **args;
+char **get_p(char *str)
+{
 	char **p;
+	str = del_quote(str, '\'', '\"');
+	p = ft_split1(str, "' ' '\t' '\n' '\v' '\f' '\r'");
+	return (p);
+}
+char **is_word(char *str, char **args, t_env **g_env, int i)
+{
+	char **p;
+	int k = 0;
 
+	if (check_space(str) && parse_env(g_env, str))
+	{
+		p = get_p(str);
+		k = 0;
+		while (p[k])
+			args[i++] = ft_strdup(p[k++]);
+		free_args(p);
+	}
+	else
+	{
+		if (ft_strcmp(str, ""))
+			args[i++] = add_args(str);
+	}
+	return (args);
+}
 
-	args = malloc(sizeof(char *) * (calculate_args(tmp) + 1));
+int parse_redir_out(int type, char *str_next, int fd)
+{
+		if (type == REDIR_OUT)
+		{
+			str_next = del_quote(str_next, '\'', '\"');
+			fd = open(str_next, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+			if (fd == -1)
+			{
+				printf("minishell: %s: ambiguous redirect\n", str_next);
+				return (1);
+			}
+		}
+		else if (type == APPEND)
+		{
+			str_next = del_quote(str_next, '\'', '\"');
+			fd = open(str_next, O_CREAT | O_WRONLY | O_APPEND, 0644);
+			if (fd == -1)
+			{
+				printf("minishell: %s: ambiguous redirect\n", str_next);
+				return(1);
+			}
+		}
+	return fd;
+}
+
+int parse_redir_in(int type, char *str_next, int fd)
+{
+	if (type == REDIR_IN)
+	{
+		str_next = del_quote(str_next, '\'', '\"');
+		fd = open(str_next, O_RDONLY, 0644);
+		if (fd == -1)
+		{
+			printf("minishell: %s: No such file or directory\n", str_next);
+			return (0);
+		}
+	}
+	if(type == HEARDOC)
+	{
+		fd = open("/tmp/srfak", O_RDONLY, 0644);
+	}
+	return (fd);
+}
+int  create(char **args, t_command **cmd, t_fd fd , int i)
+{
+	args[i] = NULL;
+	my_lstadd_back(cmd, ft_new(args, fd));
+	return (0);
+}
+char **realloc_args(char **args, int cont)
+{
+	args = malloc(sizeof(char *) * (cont + 1));
 	if(!args)
-		return ;
+		return (NULL);
+	return (args);
+}
 
+t_fd  parse_redirection(int type, char *str_next, t_fd fd)
+{
+	fd.fd_out = parse_redir_out(type, str_next, fd.fd_out);
+	fd.fd_in = parse_redir_in(type, str_next, fd.fd_in);
+	return (fd);
+}
+
+t_fd ft_fd(int fd_in, int fd_out)
+{
+	t_fd fd;
+
+	fd.fd_in = fd_in;
+	fd.fd_out = fd_out;
+	return (fd);
+}
+
+void parsing1(t_lexer *tmp ,char **args, t_env **g_env, t_command **cmd)
+{
+	int i;
+	t_fd fd;
+
+	fd = ft_fd(0, 1);
+	i = 0;
 	while (tmp != NULL)
 	{
 		if (tmp->token == WORD)
+			args = is_word(tmp->content, args, g_env, i++);
+		if(tmp->token == REDIR_OUT || tmp->token == APPEND || tmp->token == REDIR_IN || tmp->token == HEARDOC)
 		{
-			env = *g_env;
-			j = process_env(tmp, env);
-
-			if(check_space(tmp->content) && j)
-			{
-				tmp->content = del_quote(tmp->content, '\'', '\"');
-				p  = ft_split1(tmp->content, "' ' '\t' '\n' '\v' '\f' '\r'");
-				k = 0;
-				while(p[k])
-				{
-					args[i++] = ft_strdup(p[k]);
-					free(p[k++]);
-				}
-				free(p);
-			}
-			else
-			{
-				if(!ft_strcmp(tmp->content, ""))
-				{
-					tmp->content = del_quote(tmp->content, '\'', '\"');
-					args[i] = ft_strdup(tmp->content);
-					i++;
-				}
-			}
-		}
-		else if (tmp->token == REDIR_IN)
-		{
+			fd = parse_redirection(tmp->token, tmp->next->content, fd);
 			tmp = tmp->next;
-			fd.fd_in = open(tmp->content, O_RDONLY, 0644);
-			if (fd.fd_in == -1)
-			{
-				printf("minishell: %s: No such file or directory\n", tmp->content);
-				return;
-			}
 		}
-		else if (tmp->token == REDIR_OUT)
+		if (!tmp->next || tmp->token == PIPE_LINE)
 		{
-			tmp = tmp->next;
-			tmp->content = del_quote(tmp->content, '\'', '\"');
-			fd.fd_out = open(tmp->content, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-			if (fd.fd_out == -1)
-			{
-				printf("minishell: $s: ambiguous redirect\n");
-				return;
-			}
+			i = create(args, cmd, fd, i);
+			if (tmp->next && tmp->token == PIPE_LINE)
+				args = realloc_args(args, calculate_args(tmp->next));
 		}
-		else if(tmp->token == HEARDOC)
-		{
-			tmp = tmp->next;
-			fd.fd_in = open("/tmp/srfak", O_RDONLY, 0644);
-		}
-		else if (tmp->token == APPEND)
-		{
-			tmp = tmp->next;
-			tmp->content = del_quote(tmp->content, '\'', '\"');
-			fd.fd_out = open(tmp->content, O_CREAT | O_WRONLY | O_APPEND, 0644);
-			if (fd.fd_out == -1)
-			{
-				printf("minishell: $s: ambiguous redirect\n");
-				return;
-			}
-		}
-			if (!tmp->next || tmp->token == PIPE_LINE)
-			{
-				args[i] = NULL;
-				my_lstadd_back(cmd, ft_new(args, fd));
-				if (tmp->next && tmp->token == PIPE_LINE)
-				{
-					tmp = tmp->next;
-					count = calculate_args(tmp);
-					args = malloc(sizeof(char *) * (count + 1));
-					if(!args)
-						return ;
-				}
-				else
-					tmp = tmp->next;
-
-				i = 0;
-			}
-			else
-				tmp = tmp->next;
+		tmp = tmp->next;
 	}
 }
 
+void parse_args(t_lexer **list,t_command **cmd,  t_env **g_env)
+{
+	t_lexer *tmp;
+	char **args;
+
+	tmp = *list;
+	args = NULL;
+	args = realloc_args(args, calculate_args(tmp));
+	parsing1(tmp, args, g_env, cmd);
+}
+
+
+// void parsing(t_lexer **list, t_command **cmd, t_env **g_env)
+// {
+// 	t_lexer *tmp = *list;
+// 	int i = 0;
+// 	int j = 0;
+// 	t_fd fd;
+// 	int k;
+
+// 	*cmd = NULL;
+// 	fd.fd_in = 0;
+// 	fd.fd_out = 1;
+// 	char **args;
+// 	char **p;
+// 	int count = calculate_args(tmp);
+
+// 	args = malloc(sizeof(char *) * (count + 1));
+// 	if(!args)
+// 		return ;
+
+// 	while (tmp != NULL)
+// 	{
+// 		if (tmp->token == WORD)
+// 		{
+// 			j = parse_env(g_env, tmp->content);
+// 			if(check_space(tmp->content) && j)
+// 			{
+// 				tmp->content = del_quote(tmp->content, '\'', '\"');
+// 				p  = ft_split1(tmp->content, "' ' '\t' '\n' '\v' '\f' '\r'");
+// 				k = 0;
+// 				while(p[k])
+// 				{
+// 					args[i++] = ft_strdup(p[k]);
+// 					free(p[k++]);
+// 				}
+// 				free(p);
+// 			}
+// 			else
+// 			{
+
+// 				tmp->content = del_quote(tmp->content, '\'', '\"');
+// 				args[i] = ft_strdup(tmp->content);
+// 				printf("%p\n", args[i]);
+// 				i++;
+// 			}
+
+// 		}
+// 		else if (tmp->token == REDIR_IN)
+// 		{
+// 			tmp = tmp->next;
+// 			fd.fd_in = open(tmp->content, O_RDONLY, 0644);
+// 			if (fd.fd_in == -1)
+// 			{
+// 				printf("minishell: %s: No such file or directory\n", tmp->content);
+// 				return;
+// 			}
+// 		}
+// 		else if (tmp->token == REDIR_OUT)
+// 		{
+// 			tmp = tmp->next;
+// 			tmp->content = del_quote(tmp->content, '\'', '\"');
+// 			fd.fd_out = open(tmp->content, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+// 			if (fd.fd_out == -1)
+// 			{
+// 				printf("minishell: $s: ambiguous redirect\n");
+// 				return;
+// 			}
+// 		}
+// 		else if(tmp->token == HEARDOC)
+// 		{
+// 			tmp = tmp->next;
+// 			fd.fd_in = open("/tmp/srfak", O_RDONLY, 0644);
+// 		}
+// 		else if (tmp->token == APPEND)
+// 		{
+// 			tmp = tmp->next;
+// 			tmp->content = del_quote(tmp->content, '\'', '\"');
+// 			fd.fd_out = open(tmp->content, O_CREAT | O_WRONLY | O_APPEND, 0644);
+// 			if (fd.fd_out == -1)
+// 			{
+// 				printf("minishell: $s: ambiguous redirect\n");
+// 				return;
+// 			}
+// 		}
+
+// 			if (!tmp->next || tmp->token == PIPE_LINE)
+// 			{
+// 				args[i] = NULL;
+// 				my_lstadd_back(cmd, ft_new(args, fd));
+// 				if (tmp->next && tmp->token == PIPE_LINE)
+// 				{
+// 					tmp = tmp->next;
+// 					count = calculate_args(tmp);
+// 					args = malloc(sizeof(char *) * (count + 1));
+// 					if(!args)
+// 						return ;
+// 					i = 0;
+
+// 				}
+// 				else
+// 					tmp = tmp->next;
+
+// 				i = 0;
+// 			}
+// 			else
+// 				tmp = tmp->next;
+
+// 	}
+
+// }
 int valid(char *str)
 {
 	int i;
