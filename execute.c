0a2cb3afb2a2d456b57479c8d72b34e04d23fb4f
Abdeljalil El Hidaoui <abel-hid@e3r7p17.1337.ma>
2023-08-06 05:45:50 +0100

    edit

--- execute.c
+++ execute.c
@@ -6,7 +6,7 @@
 /*   By: abel-hid <abel-hid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 15:20:27 by heddahbi          #+#    #+#             */
-/*   Updated: 2023/08/06 01:04:38 by abel-hid         ###   ########.fr       */
+/*   Updated: 2023/08/06 05:44:35 by abel-hid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,27 +49,34 @@ int	ft_lst_size(t_command *cmd)
 
 void	signal_handler(int signal)
 {
-	if(exit_st.in_here_doc == 1)
+	if(exit_st.in_here_doc == 1 && signal == SIGINT)
 	{
 		close(0);
 		return ;
 	}
-	if (signal == SIGINT && ft_strcmp(rl_line_buffer,"") == 0)
-	{
-			ft_putstr_fd("\n", 1);
-			rl_on_new_line();
-			rl_replace_line("", 0);
-			rl_redisplay();
-	}
-	if(signal == SIGINT && exit_st.in_cmd == 1)
+	else if(signal == SIGINT)
 	{
 		ft_putstr_fd("\n", 1);
 		rl_on_new_line();
 		rl_replace_line("", 0);
-	}
-	if (signal == SIGQUIT)
-	{
 		if(exit_st.in_cmd == 1)
+		{
+			exit_st.status = 130;
+			exit_st.in_cmd = 0;
+		}
+		else if(exit_st.in_cmd == 0)
+		{
+			exit_st.status = 1;
+			exit_st.in_cmd = 0;
+			rl_redisplay();
+
+		}
+
+	}
+
+
+	else if (signal == SIGQUIT && exit_st.in_cmd == 1)
+	{
 			ft_putstr_fd("Quit: 3\n", 1);
 	}
 }
